887118a6ac2a969ae7291637974b7c3eff1dd2a8
Crystal Broach <crysland@gmail.com>
2020-02-29 13:54:12 -0600

    phone number validator complete

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -8,27 +8,25 @@ namespace Validator
         {
             Console.WriteLine("Validator Practice");
 
-            // TODO: Pin Checker; 
+            // Pin Validator 
             // between 4 and 8 digits, numbers only, return true if valid
+
             Console.WriteLine("Enter a new pin.  Pin must be between 4 and 8 digits, numbers only:");
             var userPin = Console.ReadLine();
             var validPin = false;
             while (!validPin)
             {
-                
                 if (userPin.Length >= 4 && userPin.Length <= 8)
                 {
                     try
                     {
                         var pinInput = int.Parse(userPin);
-
                     }
                     catch (FormatException)
                     {
                         Console.WriteLine("You entered an invalid pin, please be sure to enter a pin with 4 to 8 digits, numbers only.");
                         userPin = Console.ReadLine();
                     }
-
                     Console.WriteLine($"You entered a valid pin: {userPin}");
                     validPin = true;
                 }
@@ -39,8 +37,50 @@ namespace Validator
                 }
             }
 
-            // TODO: Phone Number Valid;  
+            // Phone Number Validator; 
             // 10 digits required; can include spaces dashes or parenthesis; does not start with 555
+
+            Console.WriteLine("Enter your phone number:");
+            var userPhoneEntry = Console.ReadLine();
+            var phoneIsValid = false;
+
+            while (!phoneIsValid)
+            {
+                char[] separator = { '(', ')', ' ', '-', };
+                String[] strlist = userPhoneEntry.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                var numberOnly = "";
+                foreach (var item in strlist) numberOnly += item;
+                var firstThree = numberOnly.Substring(0, 3);
+
+                if (numberOnly.Length == 10)
+                {
+                    if (firstThree != "555")
+                    {
+                        try
+                        {
+                            var userNumber = Int64.Parse(numberOnly);
+                            phoneIsValid = true;
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("You entered an invalid phone number, please try again.");
+                            userPhoneEntry = Console.ReadLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You entered an invalid phone number, please try again.");
+                        userPhoneEntry = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You entered an invalid phone number, please try again.");
+                    userPhoneEntry = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("You entered a valid phone number");
+
 
             // TODO: Email Valid;
             // one @ symbol; one dot after @; something before @; domain after the @; top level domain after dot
