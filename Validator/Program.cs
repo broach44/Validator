142a157a279cda332282d43571755089c0b04b6b
Crystal Broach <crysland@gmail.com>
2020-02-29 11:22:26 -0600

    Add project files.

---
+++ Validator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Validator Practice");
+
+            // TODO: Pin Checker; 
+            // between 4 and 8 digits, numbers only, return true if valid
+
+            // TODO: Phone Number Valid;  
+            // 10 digits required; can include spaces dashes or parenthesis; does not start with 555
+
+            // TODO: Email Valid;
+            // one @ symbol; one dot after @; something before @; domain after the @; top level domain after dot
+
+            // TODO: Check is name is valid Power Ranger
+
+            // TODO: Is input a palindrome
+
+            // TODO: Spongebob mocking???
+
+            Console.ReadLine();
+
+        }
+    }
+}
