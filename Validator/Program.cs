185f114f938d4a392dbd001235b356df7b2fb0d3
Crystal Broach <crysland@gmail.com>
2020-02-29 11:39:57 -0600

    pin validator working

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -10,6 +10,34 @@ namespace Validator
 
             // TODO: Pin Checker; 
             // between 4 and 8 digits, numbers only, return true if valid
+            Console.WriteLine("Enter a new pin.  Pin must be between 4 and 8 digits, numbers only:");
+            var userPin = Console.ReadLine();
+            var validPin = false;
+            while (!validPin)
+            {
+                
+                if (userPin.Length >= 4 && userPin.Length <= 8)
+                {
+                    try
+                    {
+                        var pinInput = int.Parse(userPin);
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("You entered an invalid pin, please be sure to enter a pin with 4 to 8 digits, numbers only.");
+                        userPin = Console.ReadLine();
+                    }
+
+                    Console.WriteLine($"You entered a valid pin: {userPin}");
+                    validPin = true;
+                }
+                else
+                {
+                    Console.WriteLine("You entered an invalid pin, please be sure to enter a pin with 4 to 8 digits, numbers only.");
+                    userPin = Console.ReadLine();
+                }
+            }
 
             // TODO: Phone Number Valid;  
             // 10 digits required; can include spaces dashes or parenthesis; does not start with 555
